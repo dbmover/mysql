87e1e8f2fdafc73397d663cbadd58769dcca007f
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-06 17:26:34 +0200

    load more stuff

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,15 +12,19 @@ namespace Dbmover\Mysql;
 use Dbmover\Core;
 use Dbmover\HoistDrop;
 use Dbmover\Views;
+use Dbmover\Constraints;
 
 class Plugin extends Core\Plugin
 {
     public function __construct(Core\Loader $loader)
     {
+        parent::__construct($loader);
         $loader->loadPlugins(
             Conditionals\Plugin::class,
             Views\Plugin::class,
-            Procedures::class
+            Procedures::class,
+            Constraints\Plugin::class,
+            Indexes\Plugin::class
         );
     }
 }
