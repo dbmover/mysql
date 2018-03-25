e16095b45b75be8430d48d82ba20dc6944af13f6
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-25 15:06:47 +0200

    move to new style

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,15 +3,15 @@
 /**
  * @package Dbmover
  * @subpackage Mysql
- *
- * Meta-package loading all officially supported MySQL plugins.
  */
 
 namespace Dbmover\Mysql;
 
 use Dbmover\Core;
-use Dbmover\Views;
 
+/**
+ * Meta-package loading all officially supported MySQL plugins.
+ */
 class Plugin extends Core\Plugin
 {
     public function __construct(Core\Loader $loader)
@@ -19,7 +19,7 @@ class Plugin extends Core\Plugin
         parent::__construct($loader);
         $loader->loadPlugins(
             Procedures::class,
-            Views\Plugin::class,
+            Core\Views\Plugin::class,
             Indexes\Plugin::class,
             Constraints\Plugin::class,
             Triggers\Plugin::class,
