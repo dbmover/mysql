55bc619b1fdc8877c5d216e52e75a20ab674cf06
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-25 15:12:02 +0200

    new-style procedures plugin

---
+++ src/Procedures.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package Dbmover
+ * @subpackage Mysql
+ */
+
+namespace Dbmover\Mysql;
+
+use Dbmover\Core;
+
+class Procedures extends Core\Procedures
+{
+    const DROP_ROUTINE_SUFFIX = '';
+}
+
