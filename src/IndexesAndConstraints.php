b6aeec37e74b479b93e20cb0cc09061c4b65a5f8
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-26 09:29:55 +0200

    unify plugins

--- src/IndexesAndConstraints.php
+++ src/IndexesAndConstraints.php
@@ -5,7 +5,7 @@ namespace Dbmover\Mysql;
 use Dbmover\Core;
 use PDO;
 
-class Indexes extends Core\Indexes
+class IndexesAndConstraints extends Core\IndexesAndConstraints
 {
     /**
      * @param string $sql
@@ -117,6 +117,39 @@ class Indexes extends Core\Indexes
     protected function dropPrimaryKey(string $index, string $table) : string
     {
         return "DROP INDEX `PRIMARY` ON `$table`;";
+    }
+
+    /**
+     * @param string $table
+     * @param string $contraint
+     * @param string $type
+     * @return void
+     */
+    protected function dropConstraint(string $table, string $constraint, string $type) : void
+    {
+        if ($type == 'FOREIGN KEY') {
+            $this->addOperation("ALTER TABLE $table DROP FOREIGN KEY $constraint;");
+        }
+    }
+    
+    /**
+     * @param string $table
+     * @return array
+     */
+    protected function getPrimaryKeyInfo(string $table) : array
+    {
+        static $getIndex, $getColumn;
+        if (!isset($getIndex, $getColumn)) {
+            $getIndex = $this->loader->getPdo()->prepare(
+                "SELECT column_name FROM information_schema.statistics WHERE table_schema = ? AND table_name = ? AND index_name = 'PRIMARY'");
+            $getColumn = $this->loader->getPdo()->prepare(
+                "SELECT column_type FROM information_schema.columns WHERE table_schema = ? AND table_name = ? AND column_name = ?");
+        }
+        $getIndex->execute([$this->loader->getDatabase(), $table]);
+        $column = $getIndex->fetch(PDO::FETCH_ASSOC);
+        $getColumn->execute([$this->loader->getDatabase(), $table, $column['column_name']]);
+        $column['column_type'] = $getColumn->fetchColumn();
+        return $column;
     }
 }
 
