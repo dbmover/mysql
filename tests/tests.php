9ef6086a671a27f88d167bda5b8d342fb1c87c07
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-03 15:31:42 +0100

    update unit tests to new version of toast

--- tests/tests.php
+++ tests/tests.php
@@ -1,24 +1,18 @@
 <?php
 
-namespace Dbmover\Mysql\Test;
-
-use PDO;
-use Dbmover\Mysql\Schema;
+use Dbmover\Core\Loader;
 
 /**
  * Tests for MySQL engine.
  */
-class Tests
-{
-    public function __wakeup()
-    {
-        $this->pdo = new PDO(
-            'mysql:dbname=dbmover_test',
-            'dbmover_test',
-            'moveit'
-        );
-        $this->pdo->exec(
-            <<<EOT
+return function () : Generator {
+    $pdo = new PDO(
+        'mysql:dbname=dbmover_test',
+        'dbmover_test',
+        'moveit'
+    );
+    $pdo->exec(
+        <<<EOT
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -27,43 +21,45 @@ CREATE TABLE test (
 ) ENGINE='InnoDB' DEFAULT CHARSET='UTF8';
 DROP VIEW IF EXISTS viewtest;
 EOT
-        );
-        putenv("DBMOVER_VENDOR=Mysql");
-    }
+    );
+    putenv("DBMOVER_VENDOR=Mysql");
 
     /**
-     * Initially, `test` contains three columns {?}. After we run the migration,
-     * there should be four {?}. We should also have a view called `viewtest`
-     * {?} and the inserted row has '3' for `bar` since a trigger was created
-     * during the migration {?}.
+     * Initially, `test` contains three columns. After we run the migration,
+     * there should be four. We should also have a view called `viewtest`
+     * and the inserted row has '3' for `bar` since a trigger was created
+     * during the migration.
      */
-    public function migrations(Schema &$mysql = null)
-    {
-        $mysql->__gentryConstruct(
+    yield function () use (&$pdo) {
+        $mysql = new Loader(
             'mysql:dbname=dbmover_test',
             [
                 'user' => 'dbmover_test',
                 'pass' => 'moveit',
-            ]
+                'schema' => ['tests/schema.sql'],
+                'plugins' => ["Dbmover\\Core\\ExplicitDrop", "Dbmover\\Core\\ForceNamedIndexes", "Dbmover\\Mysql\\Plugin", "Dbmover\\Core\\Data", "Dbmover\\Mysql\\Conditionals"],
+            ],
+            true
         );
-        $cols = $this->pdo->prepare(
+        $cols = $pdo->prepare(
             "SELECT * FROM INFORMATION_SCHEMA.COLUMNS
                 WHERE TABLE_SCHEMA = 'dbmover_test'
                 AND TABLE_NAME = 'test'");
         $cols->execute();
-        yield assert(count($cols->fetchAll()) == 3);
+        assert(count($cols->fetchAll()) === 3);
 
-        $mysql->addSchema(file_get_contents(__DIR__.'/schema.sql'));
-        $mysql->processSchemas();
+        $sql = $mysql->applyPlugins();
+        $mysql->applyDeferred();
+        $mysql->cleanup($sql);
 
         $cols->execute();
-        yield assert(count($cols->fetchAll()) == 4);
+        assert(count($cols->fetchAll()) === 4);
 
-        $stmt = $this->pdo->prepare("SELECT * FROM viewtest");
+        $stmt = $pdo->prepare("SELECT * FROM viewtest");
         $stmt->execute();
         $all = $stmt->fetchAll();
-        yield assert(count($all) == 1);
-        yield assert($all[0]['bar'] == 3);
-    }
-}
+        assert(count($all) === 1);
+        assert($all[0]['bar'] === 3);
+    };
+};
 
